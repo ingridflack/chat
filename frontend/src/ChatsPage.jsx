8a33b0804550ab2096ac1246713ab249a8c18aab
Ingrid Flack <ingrid_flack@hotmail.com>
2024-07-25 17:26:42 -0300

    Initial commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import {
+  PrettyChatWindow,
+} from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+     <PrettyChatWindow 
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret} 
+        style={{height: '100%'}} 
+    />
+    </div>
+  );
+};
+export default ChatsPage;
